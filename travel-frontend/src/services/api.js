59a1c0135543697f9aaac35da78926a43c29c067
Shiva singh <adarsh@mxalgo.com>
2026-02-14 14:00:15 +0530

    Connect frontend to deployed backend

--- travel-frontend/src/services/api.js
+++ travel-frontend/src/services/api.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const API = axios.create({
-  baseURL: "http://localhost:5000/api",
+  baseURL: "https://travel-backend-pzxk.onrender.com/api",
 });
 
 // Attach token automatically
